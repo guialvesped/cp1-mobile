4dd3b31638adcda7b4c1e7a64c65841cc1ec227a
Guilherme Alves Pedroso <alvespedrosog@gmail.com>
2025-03-14 20:55:46 -0300

    Fix calculate result

--- src/screen/Result.js
+++ src/screen/Result.js
@@ -1,11 +1,30 @@
-import { StyleSheet, View } from 'react-native';
-import Calculator from '../components/Calculator';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function Result({ route }) {
     const produto = route.params.produto
+    const { name, value, discountValue } = produto
+    const calculatedDiscount = value * (discountValue / 100)
+    const finalValue = value + calculatedDiscount
     return (
         <View style={styles.container}>
-            <Calculator produto={produto} />
+
+            <View>
+                <Text>
+                    Nome: {name}
+                </Text>
+                <Text>
+                    Valor original: {value}
+                </Text>
+                <Text>
+                    Percentual de aumento: {discountValue}%
+                </Text>
+                <Text>
+                    Valor de aumento: R$ {calculatedDiscount}
+                </Text>
+                <Text>
+                    Valor final: {finalValue}
+                </Text>
+            </View>
         </View>
     );
 }
